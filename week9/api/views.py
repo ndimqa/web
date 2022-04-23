eff1abf943fa6b8dd2958f69c828631f7e3965b0
Dinmukhamed <dinmukhamednuran@gmail.com>
2022-04-23 19:04:59 +0600

    LAB 10

--- week9/api/views.py
+++ week9/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import action
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from api.serializers import CompanySerializer, VacancySerializer
 from api.models import Company, Vacancy
@@ -8,6 +9,7 @@ from api.models import Company, Vacancy
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+    permission_classes = (IsAuthenticated, )
 
     @action(methods=['GET'], detail=True, serializer_class=VacancySerializer)
     def vacancies(self, request, pk=None):
@@ -20,6 +22,7 @@ class CompanyViewSet(viewsets.ModelViewSet):
 class VacancyViewSet(viewsets.ModelViewSet):
     queryset = Vacancy.objects.all()
     serializer_class = VacancySerializer
+    permission_classes = (IsAuthenticated,)
 
     @action(detail=False, methods=['GET'])
     def top_ten(self, request):
